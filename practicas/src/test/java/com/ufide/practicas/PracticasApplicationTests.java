d23e5c5360d1e5f58a5c665f3bd1eefea775df2c
Jean8099324 <124542833+Jean8099324@users.noreply.github.com>
2026-06-19 01:29:13 -0600

    Practica semana1

---
+++ practicas/src/test/java/com/ufide/practicas/PracticasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufide.practicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
